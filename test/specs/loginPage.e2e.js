2e1672767458ae9e3bba6ece4de56524d0214d07
Dima Smaha <fannifty@gmail.com>
2023-11-29 19:04:36 +0200

    dur

--- test/specs/loginPage.e2e.js
+++ test/specs/loginPage.e2e.js
@@ -5,6 +5,6 @@ describe('Login/Sign Up Form', () => {
     it('should signup with valid credentials', async () => {
         await loginPage.signUp('testing123@mailnator.com', 'SuperSecretPassword!')
         await expect(loginPage.popupSignupSuccessMessage).toBeExisting()
-        await expect(loginPage.popupSignupSuccessMessage).getText('You successfully signed up!')
+        await expect(loginPage.popupSignupSuccessMessage).toHaveTextContaining('You successfully signed up!')
     })
 })
