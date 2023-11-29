d56e1b8b33616f93caa4a12e46a511a854d7430f
Dima Smaha <fannifty@gmail.com>
2023-11-29 20:55:40 +0200

    TEXT

--- test/specs/webviewPage.e2e.js
+++ test/specs/webviewPage.e2e.js
@@ -3,6 +3,7 @@ const webviewPage = require("../pageobjects/webviewPage.page");
 describe("Webview Page", () => {
   it("should verify webview page", async () => {
     (await webviewPage.webviewWidget).click();
+    (await webviewPage.viewOnGH).scrollIntoView();
     await expect(webviewPage.viewOnGH).toBeDisplayed();
   });
 });
